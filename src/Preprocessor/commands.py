239c18696922c50b7b58d3618f25c9e8c3beb0aa
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-01-01 19:15:55 +0100

    Added tests for basic define

--- src/Preprocessor/commands.py
+++ src/Preprocessor/commands.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from .preprocessor import *
+from datetime import datetime
+
+from .preprocessor import Preprocessor
 
 
-def define(processor: Preprocessor, args_string : str) -> str:
-	match = re.match(REGEX_IDENTIFIER, args_string)
-	if match is None:
-		raise CommandError(Position("", 0, 0), "Define has no valid command name")
-	# identifier = match.group()
+def define(preprocessor: Preprocessor, args_string : str) -> str:
+	ident, text = preprocessor.get_identifier_name(args_string)
+	if ident == "":
+		preprocessor.send_error("def does not start with a valid identifier")
 
+	# removed trailing\leading whitespace
+	text = text.strip()
+
+
+	if len(text) >= 2 and text[0] == '"' and text[-1] == '"':
+		text = text[1:-1].encode().decode("unicode-escape")
+
+
+	def defined_command(p: Preprocessor, s: str) -> str:
+		return p.parse(text)
+	defined_command.__doc__ = """Defined command for {}""".format(ident)
+	preprocessor.commands[ident] = defined_command
 	return ""
+
+def date(p: Preprocessor, s: str) -> str:
+	x = datetime.now()
+	return "{:04}-{:02}-{:02}".format(x.year, x.month, x.day)
+
+Preprocessor.commands["def"] = define
+Preprocessor.commands["date"] = date
---
+++ tests/test_commands.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from Preprocessor import Preprocessor
+
+
+class TestCommands:
+
+	pre = Preprocessor()
+
+	def test_def(self):
+		test = [
+			("{% def nom jean %}\nbonjour je suis {% nom %}","\nbonjour je suis jean"),
+			("{% def nom\" jean\" %}\nbonjour je suis {% nom %}","\nbonjour je suis  jean"),
+			("{% def nom\" \"\\\"jean\" %}\nbonjour je suis {% nom %}","\nbonjour je suis  \"\"jean"),
+
+		]
+		for in_str, out_str in test:
+			assert self.pre.parse(in_str) == out_str
