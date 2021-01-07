43c3121a5dfff4a29dde67a2920573ff6551bea2
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-01-08 00:42:30 +0100

    Renamed some variables

--- src/preprocessor/final_actions.py
+++ src/preprocessor/final_actions.py
@@ -14,10 +14,10 @@ def fnl_strip_empty_lines(_: Preprocessor, string: str) -> str:
 	"""final action to remove empty lines (containing whitespace only) from the text"""
 	return re.sub(r"\n\s*\n", "\n", string)
 
-def cmd_strip_empty_lines(preprocessor: Preprocessor, s: str) -> str:
+def cmd_strip_empty_lines(preprocessor: Preprocessor, args: str) -> str:
 	"""the strip_empty_lines command
 	queues fnl_strip_empty_lines to preprocessor final actions"""
-	if s.strip() != "":
+	if args.strip() != "":
 		preprocessor.send_warning("strip_empty_line takes no arguments")
 	preprocessor.add_finalaction(fnl_strip_empty_lines)
 	return ""
@@ -26,10 +26,10 @@ def fnl_strip_leading_whitespace(_: Preprocessor, string: str) -> str:
 	"""final action to remove leading whitespace (indent) from string"""
 	return re.sub("^[ \t]*", "", string, flags = re.MULTILINE)
 
-def cmd_strip_leading_whitespace(preprocessor: Preprocessor, s: str) -> str:
+def cmd_strip_leading_whitespace(preprocessor: Preprocessor, args: str) -> str:
 	"""the strip_leading_whitespace command
 	queues fnl_strip_leading_whitespace to preprocessor final actions"""
-	if s.strip() != "":
+	if args.strip() != "":
 		preprocessor.send_warning("strip_leading_whitespace takes no arguments")
 	preprocessor.add_finalaction(fnl_strip_leading_whitespace)
 	return ""
@@ -38,10 +38,10 @@ def fnl_strip_trailing_whitespace(_: Preprocessor, string: str) -> str:
 	"""final action to remove trailing whitespace (indent) from string"""
 	return re.sub("[ \t]*$", "", string, flags = re.MULTILINE)
 
-def cmd_strip_trailing_whitespace(preprocessor: Preprocessor, s: str) -> str:
+def cmd_strip_trailing_whitespace(preprocessor: Preprocessor, args: str) -> str:
 	"""the strip_trailing_whitespace command
 	queues fnl_strip_trailing_whitespace to preprocessor final actions"""
-	if s.strip() != "":
+	if args.strip() != "":
 		preprocessor.send_warning("strip_trailing_whitespace takes no arguments")
 	preprocessor.add_finalaction(fnl_strip_trailing_whitespace)
 	return ""
@@ -58,10 +58,10 @@ def fnl_fix_last_line(_: Preprocessor, string: str) -> str:
 		string = string[:ii+2]
 	return string
 
-def cmd_fix_last_line(preprocessor: Preprocessor, s: str) -> str:
+def cmd_fix_last_line(preprocessor: Preprocessor, args: str) -> str:
 	"""the fix_last_line command
 	queues fnl_fix_last_line to preprocessor final actions"""
-	if s.strip() != "":
+	if args.strip() != "":
 		preprocessor.send_warning("fix_last_line takes no arguments")
 	preprocessor.add_finalaction(fnl_fix_last_line)
 	return ""
@@ -81,10 +81,10 @@ def fnl_fix_first_line(_: Preprocessor, string: str) -> str:
 			break
 	return string
 
-def cmd_fix_first_line(preprocessor: Preprocessor, s: str) -> str:
+def cmd_fix_first_line(preprocessor: Preprocessor, args: str) -> str:
 	"""the fix_first_line command
 	queues fnl_fix_first_line to preprocessor final actions"""
-	if s.strip() != "":
+	if args.strip() != "":
 		preprocessor.send_warning("fix_last_line takes no arguments")
 	preprocessor.add_finalaction(fnl_fix_first_line)
 	return ""
@@ -105,16 +105,16 @@ replace_parser.add_argument("--count", "-c", nargs='?', default=0, type=int)
 replace_parser.add_argument("pattern")
 replace_parser.add_argument("replacement")
 
-def cmd_replace(p: Preprocessor, args: str) -> str:
+def cmd_replace(preprocessor: Preprocessor, args: str) -> str:
 	"""the replace command
 	usage: replace [-r|--regex] [-i|--ignore-case] [-w|--whole-word]
 	               [-c|--count <number>] pattern replacement
 	"""
-	split = p.split_args(args)
+	split = preprocessor.split_args(args)
 	try:
 		arguments = replace_parser.parse_args(split)
 	except argparse.ArgumentError:
-		p.send_error(
+		preprocessor.send_error(
 			"invalid argument.\n"
 			"usage: replace [-r|--regex] [-i|--ignore-case] [-w|--whole-word]"
 			"               [-c|--count <number>] pattern replacement")
@@ -125,7 +125,7 @@ def cmd_replace(p: Preprocessor, args: str) -> str:
 		flags |= re.IGNORECASE
 	if arguments.regex:
 		if arguments.whole_word:
-			p.send_error("incompatible arguments : --regex and --whole-word")
+			preprocessor.send_error("incompatible arguments : --regex and --whole-word")
 	else:
 		pattern = re.escape(pattern)
 		if arguments.whole_word:
@@ -133,18 +133,18 @@ def cmd_replace(p: Preprocessor, args: str) -> str:
 			repl = "\\1{}\\3".format(repl)
 	count = arguments.count
 	if count < 0:
-		p.send_error("invalid argument.\nthe replace --count argument must be positive")
-	pos = p.current_position.cmd_begin
+		preprocessor.send_error("invalid argument.\nthe replace --count argument must be positive")
+	pos = preprocessor.current_position.cmd_begin
 
-	def fnl_replace(p: Preprocessor, string: str) -> str:
+	def fnl_replace(preprocessor: Preprocessor, string: str) -> str:
 		try:
 			return re.sub(pattern, repl, string, count=count, flags = flags)
 		except re.error as err:
-			p.context.update(pos)
-			p.send_error("replace regex error: {}".format(err.msg))
-			p.context.pop()
+			preprocessor.context.update(pos)
+			preprocessor.send_error("replace regex error: {}".format(err.msg))
+			preprocessor.context.pop()
 			return ""
 	fnl_replace.__name__ = "fnl_replace_lambda"
 	fnl_replace.__doc__ = "final action for replace {}".format(args)
-	p.add_finalaction(fnl_replace)
+	preprocessor.add_finalaction(fnl_replace)
 	return ""
