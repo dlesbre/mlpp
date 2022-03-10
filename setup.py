4a94d37214d335cdffbdbfe1f80633c89766b8c0
Dorian Lesbre <dorian.lesbre@gmail.com>
2022-03-11 00:38:17 +0100

    More setup.py details

--- setup.py
+++ setup.py
@@ -23,15 +23,31 @@ setup(
 	python_requires = ">=3.6",
 	license = "MIT",
 	platforms=["any"],
+	keywords=['preprocessor preprocess markup language python terminal'],
 	classifiers = [
+		# How mature is this project? Common values are
+		#   3 - Alpha
+		#   4 - Beta
+		#   5 - Production/Stable
+		'Development Status :: 5 - Production/Stable',
+
+		# Indicate who your project is intended for
+		'Intended Audience :: Developers',
+		'Environment :: Console',
+		'Natural Language :: English',
+
+		# Pick your license as you wish (should match "license" above)
+		"License :: OSI Approved :: MIT License",
+
 		"Programming Language :: Python :: 3",
 		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
 		"Programming Language :: Python :: 3.8",
 		"Programming Language :: Python :: 3.9",
 		"Programming Language :: Python :: 3.10",
-		"License :: OSI Approved :: MIT License",
+
 		"Operating System :: OS Independent",
 		"Topic :: Utilities",
 	],
+	data_files=[('', ['LICENSE'])],
 )
